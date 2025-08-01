e2a53f15145e2c39aabef2b7639575c48b51326d
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-08-01 19:30:09 +0500

    [+] added day 4

---
+++ day/day 5 - operators/index.js
@@ -0,0 +1,9 @@
+let strA = "Your name is";
+
+let username = prompt("Enter name:");
+let age = prompt("Enter age");
+
+// console.log(strA, username, "Your age is", age)
+console.log(`Your name is ${username}. Your age is ${age}`);
+
+// your name is ARK, your age is 18;
